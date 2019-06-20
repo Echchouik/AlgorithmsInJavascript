64cab3f11a93c234741463647fdca36fc6b2a70b
Echchouik <yassine.echchouik@gmail.com>
2019-06-20 11:53:58 +0200

    first part of the algorithm

--- harmlessRansomNote.js
+++ harmlessRansomNote.js
@@ -5,3 +5,18 @@
  if we do the functio will return true
  else it will return false
  */
+
+ function harmlessRansomNote (noteText, magazineText) {
+     var noteArr = noteText.split(' ');
+     var magazineArr = magazineText.split(' '); 
+     var magazineObj = {};
+
+     magazineArr.forEach(word => {
+         if (!magazineObj[word]) magazineObj[word] = 0;
+         magazineObj[word]++;
+     });
+
+     console.log(magazineObj);
+ }
+
+ harmlessRansomNote('', 'this is all the magazine text in the magazine');
