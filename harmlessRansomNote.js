65ab44a3582f9ec578313822bfe1f52af7221fb9
Echchouik <yassine.echchouik@gmail.com>
2019-06-20 11:22:51 +0200

    The Harmless Ransom Note algorithm Doc

---
+++ harmlessRansomNote.js
@@ -0,0 +1,7 @@
+"use strict"
+ /*
+ This function will made a note from a magazine text
+ The purpose is to tell us if we have enough words in the magazine to create note
+ if we do the functio will return true
+ else it will return false
+ */
